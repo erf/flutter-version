4b5f1ce235ed0779736e7534b1ff5178457b73df
Erlend Lind Madsen <erlendf80@gmail.com>
2024-08-14 19:39:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Flutter Version Switcher
+
+This Bash script helps you easily switch between different versions of Flutter using Git tags specified in a `.flutter-version` file.
+
+## Prerequisites
+
+- **Flutter**: Ensure that Flutter is installed on your system and is accessible via the command line.
+- **Git**: The script requires Git to be installed to switch between versions using tags.
+
+## Getting Started
+
+### 1. Create a `.flutter-version` File
+
+In the root directory of your project, create a file named `.flutter-version`. This file should contain the Git tag of the Flutter version you want to use. For example:
+
+3.19.0
---
+++ set-flutter-version.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check if .flutter-version file exists
+if [[ -f ".flutter-version" ]]; then
+  # Read the version tag from the file
+  FLUTTER_TAG=$(<.flutter-version)
+
+  # Find the Flutter installation path
+  FLUTTER_PATH=$(which flutter)
+
+  if [[ -z "$FLUTTER_PATH" ]]; then
+    echo "Flutter is not installed or not found in your PATH."
+    exit 1
+  fi
+
+  # Get the directory of the Flutter installation
+  FLUTTER_DIR=$(dirname "$(dirname "$FLUTTER_PATH")")
+
+  # Change to the Flutter directory
+  cd "$FLUTTER_DIR" || exit
+
+  # Checkout the specified tag
+  git checkout "$FLUTTER_TAG"
+  
+  if [[ $? -ne 0 ]]; then
+    echo "Failed to checkout the tag $FLUTTER_TAG."
+    exit 1
+  else
+    echo "Successfully checked out Flutter version $FLUTTER_TAG."
+  fi
+else
+  echo ".flutter-version file not found."
+  exit 1
+fi
+
